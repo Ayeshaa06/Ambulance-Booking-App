a25e6d32123e694653c8090f865ec1d35c7669a3
JadhavHarsh306 <harshdilipjadhav306@gmail.com>
2025-01-04 09:20:34 +0530

    Landing Page

--- src/components/RequestAmbulance.js
+++ src/components/RequestAmbulance.js
@@ -20,10 +20,7 @@ const RequestAmbulance = () => {
         </div>
       </div>
       <div className="image-container">
-        <img
-          src="./images/Map.jpg"
-          alt="Google Map"
-        />
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3782.992638906185!2d73.83082851077073!3d18.529234782493262!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bc2bf4a06f8ec33%3A0x8fbd07f290a4163a!2sIET%20C-DAC%20ACTS%20ATC%2C%20Pune!5e0!3m2!1sen!2sin!4v1734544091448!5m2!1sen!2sin&output=embed" title="Google-Map"></iframe>
       </div>
     </div>
   );
